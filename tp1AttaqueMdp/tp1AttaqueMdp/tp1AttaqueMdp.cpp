ba20f1f5aee304b739dedeb41824a08afd7b9bfc
Louka Morneau <morneaulouka@hotmail.com>
2019-02-03 13:53:43 -0500

    Ajout de MD5 + début creation de mots
    Trouvé MD5 cpp et h sur http://www.zedwood.com/article/cpp-md5-function
    
    je commence la création de mots

--- tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
+++ tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
@@ -6,13 +6,13 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <openssl\md5.h>
+#include "md5.h"
 
 #include <ctime>
 
 using namespace std;
 
-char m_dispo[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*' };
+const char m_dispo[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*' };
 
 
 int ChargerListeDictionnaire(string pathFichier, vector<string>* listeARemplir)
@@ -27,17 +27,14 @@ int ChargerListeDictionnaire(string pathFichier, vector<string>* listeARemplir)
 	{
 		while (inputFileStream >> mot)
 		{
-			//retour->push_back(mot);
 			listeARemplir->push_back(mot);
-			//cout << mot << "\n";//prends beaucoup de temps
 			nbMot++;
 		}
 	}
 
+	cout << "Temps chargement dictionnaire: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
 	cout << "Nombre de mots: " << nbMot << "\r\n";
 
-	cout << "Temps chargement dictionnaire: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
-	
 	return nbMot;
 }
 
@@ -47,54 +44,48 @@ int HashListe(vector<string>* listeClair, vector<string>* listeHash, int nbMots)
 
 	for (int i = 0; i < nbMots; i++)
 	{
-		string mot = listeClair->at(i);
+		listeHash->push_back(md5(listeClair->at(i)));
 	}
-	//for (int i = 0; i < listeClair.size(); i++)
-	//{
-	//	/*unsigned char result[MD5_DIGEST_LENGTH];
-	//	const unsigned char* str;
-	//	str = (unsigned char*)"hello";
-	//	unsigned int long_size = 100;
-	//	MD5(str,long_size,result);*/
-	//	string mot = listeClair.at(i);
-
-	//	unsigned char result[MD5_DIGEST_LENGTH];
-	//	const unsigned char* str;
-	//	//str = (const unsigned char*)listeClair->at(i);
-	//	//MD5(str, listeClair[i].size, result);
-
-	//	string r((char*)result);
-
-	//}
 
 	cout << "Temps hashage dictionnaire: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
 
 	return 0;
 }
 
-//
-//vector<string> CreeListeMots(int nbreCharMax)//8 max
-//{
-//	vector<string> liste;
-//
-//	size_t i = strlen(m_dispo);
-//
-//	return liste;
-//}
+int CreeListeMots(int nbreCharMax, vector<string>* listeARemplir)//8 max
+{
+	int nbCharDispo = strlen(m_dispo);
+	unsigned long long int nbMot = pow(nbCharDispo, nbreCharMax);
+	vector<char> *mot = new vector<char>();
+	
+	for (int i = 0; i < nbreCharMax; i++)
+	{
+		mot->push_back(NULL);
+	}
+
+	for (int i = 0; i < nbCharDispo; i++)
+	{
+		mot->at(0) = m_dispo[i];
+	}
+
+
+	return nbMot;
+}
 
 int main()//Je regardes pour ajouter openNp (Multithreading) pour que ça soit plus rapide
 {
-	vector<string> *listeMots = new vector<string>();
+	vector<string> *listeMot = new vector<string>();
 	vector<string> *listeHash = new vector<string>();
-	//vector<vector<string>> listeSolution; //ex:[ [hamburger, 6f31ce91f67bf9246ea237850686cc7a], [citronade, 6f31ce91f67bf9246ea23785068765ae], ...]
+	vector<string> *listeMotCree = new vector<string>();
 	
+	CreeListeMots(8, listeMotCree);
 	
 	//Charge dictionnaire
-	int nbMots = ChargerListeDictionnaire("mots-8-et-moins.txt", listeMots);
-	if (nbMots != 0)
+	int nbMot = ChargerListeDictionnaire("mots-8-et-moins.txt", listeMot);
+	if (nbMot != 0)
 	{
 		//Hash dictionnaire
-		HashListe(listeMots, listeHash, nbMots);
+		HashListe(listeMot, listeHash, nbMot);
 	}
 
 
