1e5fe959ff5948584adda70be80631c6ed405bf2
Louka Morneau <morneaulouka@hotmail.com>
2019-02-04 11:53:19 -0500

    Finition des deux parties
    Ajout de finitions pour les deux parties
    +
    Enlève le code pas utile

--- tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
+++ tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
@@ -1,22 +1,21 @@
 // tp1AttaqueMdp.cpp : Définit le point d'entrée pour l'application console.
-//
+//Louka Morneau
+//2019-02-04
+//Cours de sécurité informatique de l'UQAR
 
 #include "stdafx.h"
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include "md5.h"
+#include <iostream>//cin,cout
+#include <fstream>//ofstream
+#include <string>//string
+#include <vector>//vector
+#include <ctime>//clock
 
-#include <ctime>
-
-#include "compteur.h"
+#include "md5.h"//md5
+#include "compteur.h"//compteur
 
 using namespace std;
 
-const char m_dispo[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','&','*' };
-
-
+//Prends chaque mot d'un fichier et les ajoute dans listeARemplir.
 int ChargerListeDictionnaire(string pathFichier, vector<string>* listeARemplir)
 {
 	const clock_t temps = clock();
@@ -40,7 +39,8 @@ int ChargerListeDictionnaire(string pathFichier, vector<string>* listeARemplir)
 	return nbMot;
 }
 
-int HashListe(vector<string>* listeClair, vector<string>* listeHash, int nbMots)
+//Hash une liste de mot et retourne le résultat dans listHash.
+void HashListe(vector<string>* listeClair, vector<string>* listeHash, int nbMots)
 {
 	const clock_t temps = clock();
 
@@ -51,102 +51,66 @@ int HashListe(vector<string>* listeClair, vector<string>* listeHash, int nbMots)
 
 	cout << "Temps hashage dictionnaire: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
 
-	return 0;
+	return;
 }
 
-//Crée une liste avec tous les mots possible d'une longueur indiquée
-//https://codereview.stackexchange.com/questions/166571/create-all-combinations-of-length-n-from-a-given-alphabet-2
-int CreeListeMots(int nbreCharMax, vector<string>* listeARemplir)//8 max
+//Vérifie si un hash donné avec son mot respectif est dans la liste des hash à trouver. Si oui, ajoute la combinaison au fichier de réponse.
+void VerifHashDansListe(string hash, string mot, vector<string>* listeHashATrouver)
 {
-	const clock_t temps = clock();
-
-	int nbCharDispo = strlen(m_dispo);
-	unsigned long long int nbMot = pow(nbCharDispo, nbreCharMax);
-
-	vector<int> index(nbreCharMax, 0);
-
-	for(;;)
+	if (find(listeHashATrouver->begin(), listeHashATrouver->end(), hash) != listeHashATrouver->end())
 	{
-		string mot(index.size(), ' ');
-		for (unsigned long long int i = 0; i < index.size(); ++i)
-		{
-			mot[i] = m_dispo[index[i]];
-		}
-		listeARemplir->push_back(mot);
-
-		for (unsigned long long int i = index.size() - 1; ; --i)
-		{
-			if (i < 0)
-			{
-				cout << "Temps de création de mots de longueur " << nbreCharMax << ": " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
-				return nbMot;
-			}
-
-			++index[i];
-
-			if (index[i] == nbCharDispo)
-			{
-				index[i] = 0;
-			}
-			else
-			{
-				break;
-			}
-		}
+		ofstream fichierReponse;
+		fichierReponse.open("reponse.txt", ios_base::app);
+		fichierReponse << mot << " - " << hash << "\n";
+		fichierReponse.close();
+		cout << "Correspondance trouvée: " << mot << " - " << hash << "\n";
 	}
 }
 
-//Ajoute une liste de mots dans un fichier spécifié
-bool AjouteListeMotsDansFichierTexte(string pathFichier, vector<string>* listeAAjouter)
+
+int main()
 {
 	const clock_t temps = clock();
 
-	ofstream openFileStream;
-	openFileStream.open(pathFichier, ios_base::app);
+	//load les hash à trouver (Partie 1 et 2)
+	vector<string>* listeHashATrouver = new vector<string>();
+	ChargerListeDictionnaire("hashs-a-trouver.txt", listeHashATrouver);
 
-	for (unsigned long long int i = 0; i < listeAAjouter->size(); i++)
+	//Partie 1
+	cout << "Partie 1:\n";
+	vector<string> *listeMot = new vector<string>();
+	vector<string> *listeHash = new vector<string>();
+
+	//Charge dictionnaire
+	int nbMot = ChargerListeDictionnaire("mots-8-et-moins.txt", listeMot);
+	if (nbMot != 0)
 	{
-		openFileStream << listeAAjouter->at(i) << "\n";
+		//Hash dictionnaire
+		HashListe(listeMot, listeHash, nbMot);
 	}
 
-	openFileStream.close();
+	for (int i = 0; i < nbMot; i++)
+	{
+		VerifHashDansListe(listeHash->at(i), listeMot->at(i), listeHashATrouver);
+	}
 
-	cout << "Temps d'ajout de la liste de mot au fichier: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
-
-	return true;
-}
-
-int main()//Je regardes pour ajouter openNp (Multithreading) pour que ça soit plus rapide
-{
-	//vector<string> *listeMot = new vector<string>();
-	//vector<string> *listeHash = new vector<string>();
-	//
-	//for (int i = 1; i <= 8; i++)//8max
-	//{
-	//	vector<string> *listeMotCree = new vector<string>();
-	//	CreeListeMots(i, listeMotCree);
-	//	AjouteListeMotsDansFichierTexte("mots-8-et-moins-crees.txt", listeMotCree);
-	//}
-	//
-	//////Charge dictionnaire
-	////int nbMot = ChargerListeDictionnaire("mots-8-et-moins.txt", listeMot);
-	////if (nbMot != 0)
-	////{
-	////	//Hash dictionnaire
-	////	HashListe(listeMot, listeHash, nbMot);
-	////}
+	cout << "Temps d'exécution partie 1: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
+	
+	//Partie 2 //Je regardes pour ajouter openNp pour que ça soit plus rapide
+	cout << "Partie 2:\n";
 	string dispo = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
-	size_t longueurMax = 3;
+	size_t longueurMax = 8;
+	Compteur c(longueurMax, dispo);//Outils de création de mots
 
-	Compteur c(longueurMax, dispo);//8 max
-
-	int i = 0;
-	while(c.Increment())
+	do
 	{
-		//Hash le mot
-		//vérifie si correspond à un ou plusieurs mots dans la table
-		i++;
-	}
+		string mot = c.GetValue();
+		VerifHashDansListe(md5(mot), mot, listeHashATrouver);
+	} while (c.Increment());//Tant que pas d'overflow
+	
+	cout << "Temps d'exécution partie 2: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
+
+	cout << "Temps d'exécution total: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
 
 	//Pour pauser à la fin
 	string s;
