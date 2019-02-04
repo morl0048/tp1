206b56853fd6145bd05747c897094b430df3235d
Louka Morneau <morneaulouka@hotmail.com>
2019-02-04 14:32:06 -0500

    remove comment

--- tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
+++ tp1AttaqueMdp/tp1AttaqueMdp/tp1AttaqueMdp.cpp
@@ -96,7 +96,7 @@ int main()
 
 	cout << "Temps d'exécution partie 1: " << float(clock() - temps) / CLOCKS_PER_SEC << "\r\n";
 	
-	//Partie 2 //Je regardes pour ajouter openMp pour que ça soit plus rapide
+	//Partie 2
 	cout << "Partie 2:\n";
 	string dispo = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%&*";
 	size_t longueurMax = 8;
